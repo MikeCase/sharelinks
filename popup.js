ba06d026dcf46ce0fe3b84833fc833600ef4c2bb
Mike Case <michael.e.case@gmail.com>
2021-01-31 14:03:07 -0600

    Initial Commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        var url = tabs[0].url;
+        var qrcode = new QRCode(document.getElementById('qrcode'), url);
+        var link = document.getElementById('url').innerText = url;
+    });
+
+});
