afe93b8f1ff9ee4d6977f8220334b95c0f7887fd
Mike Case <michael.e.case@gmail.com>
2021-01-31 19:34:29 -0600

    adding options page

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     chrome.tabs.query({active: true, currentWindow: true}, tabs => {
         var url = tabs[0].url;
         var qrcode = new QRCode(document.getElementById('qrcode'), url);
-        var link = document.getElementById('url').innerText = url;
+        // var link = document.getElementById('url').innerText = url;
     });
 
 });
